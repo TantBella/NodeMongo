299d5d9ae18fd2e27077ec8379eb891c95b91172
Bella Solo <bella.solomon-falkfrii@outlook.com>
2025-01-20 15:01:40 +0100

    a little bit of crud

--- server.js
+++ server.js
@@ -11,17 +11,45 @@ app.get("/", (req, res) => {
   res.send("Welcome to my MongoApi");
 });
 
-// app.post("/blogpost", (req, res) => {
-//   res.send("This is postreq");
-// });
+app.get("/api/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.get("/api/product/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
 app.post("/api/product", async (req, res) => {
   try {
     const product = await Product.create(req.body);
-res.status(200).json(product)
+    res.status(200).json(product);
   } catch (error) {
-console.log(error.message);
-res.status(404)
+    console.log(error.message);
+    res.status(404);
+  }
+});
+
+app.put("/api/product/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body);
+    if(!product){
+      return res.status(400).json({message: "pro doesnt exist"})
+    }
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
   }
 });
 
