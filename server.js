5a2650bd521585379291be5aab1475b97d9b91e1
Bella Solo <bella.solomon-falkfrii@outlook.com>
2025-01-20 11:08:26 +0100

    express and mongoose

--- server.js
+++ server.js
@@ -2,8 +2,16 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+// console.log("Hello World!");
+
+app.get("/", (req, res) => {
+  res.send("Welcome to my MongoApi");
+});
+
+app.post("/blogpost", (req, res) => {
+  res.send("This is postreq");
+});
+
 app.listen(PORT, () => {
   console.log("listening to 3000");
 });
-
-console.log("Hello World!");
