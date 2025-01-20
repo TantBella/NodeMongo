41ef09179e135e2398b39fd8bdc29fbe462538dc
Bella Solo <bella.solomon-falkfrii@outlook.com>
2025-01-20 13:44:01 +0100

    added productschema

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const PORT = 3000;
 
-// console.log("Hello World!");
 
 app.get("/", (req, res) => {
   res.send("Welcome to my MongoApi");
@@ -13,13 +13,9 @@ app.post("/blogpost", (req, res) => {
   res.send("This is postreq");
 });
 
-mongoose
-  .connect(
-    "mongodb+srv://tantbella:gCsEmiXzrO6hGscf@bellasapi.dqh0j.mongodb.net/Products-Api?retryWrites=true&w=majority&appName=BellasAPI"
-  )
-  .then(() => {
-    console.log("connected to mongo");
-    app.listen(PORT, () => {
-      console.log("listening to 3000");
-    });
+mongoose.connect(process.env.MONGO_URI).then(() => {
+  console.log("connected to mongo");
+  app.listen(PORT, () => {
+    console.log("listening to 3000");
   });
+});
