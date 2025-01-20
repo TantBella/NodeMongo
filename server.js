c4db08a4b8acfcd59714a483a4f86308a2e7af0d
Bella Solo <bella.solomon-falkfrii@outlook.com>
2025-01-20 11:48:22 +0100

    db/connection

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 const PORT = 3000;
 
 // console.log("Hello World!");
@@ -12,6 +13,13 @@ app.post("/blogpost", (req, res) => {
   res.send("This is postreq");
 });
 
-app.listen(PORT, () => {
-  console.log("listening to 3000");
-});
+mongoose
+  .connect(
+    "mongodb+srv://tantbella:gCsEmiXzrO6hGscf@bellasapi.dqh0j.mongodb.net/Products-Api?retryWrites=true&w=majority&appName=BellasAPI"
+  )
+  .then(() => {
+    console.log("connected to mongo");
+    app.listen(PORT, () => {
+      console.log("listening to 3000");
+    });
+  });
