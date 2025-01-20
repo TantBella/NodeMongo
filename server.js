64173e08cde6aee0035b36e22f3be3a79c7844ca
Bella Solo <bella.solomon-falkfrii@outlook.com>
2025-01-20 14:42:22 +0100

    made postreq

--- server.js
+++ server.js
@@ -3,14 +3,26 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const PORT = 3000;
+const Product = require("./models/productModels");
 
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Welcome to my MongoApi");
 });
 
-app.post("/blogpost", (req, res) => {
-  res.send("This is postreq");
+// app.post("/blogpost", (req, res) => {
+//   res.send("This is postreq");
+// });
+
+app.post("/api/product", async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+res.status(200).json(product)
+  } catch (error) {
+console.log(error.message);
+res.status(404)
+  }
 });
 
 mongoose.connect(process.env.MONGO_URI).then(() => {
